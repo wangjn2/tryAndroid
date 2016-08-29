0fca6c0a452225f23bd9f5626aa5aff619595fdc
jnw <wangjn2@163.com>
2016-08-29 23:40:56 +0800

    Intent传值

--- TryGradle/app/src/main/java/jnw/android/trygradle/HelloWorldActivity.java
+++ TryGradle/app/src/main/java/jnw/android/trygradle/HelloWorldActivity.java
@@ -1,14 +1,29 @@
 package jnw.android.trygradle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class HelloWorldActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "jnw.android.trygradle.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // 中文测试
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
     }
 }
