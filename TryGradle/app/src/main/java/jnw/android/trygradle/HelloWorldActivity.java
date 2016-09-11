414ac2ce38b45a07d945c11f3c3740744c1f77f8
JNW <wangjn2@163.com>
2016-09-11 15:51:43 +0800

    try startActivityForResult

--- TryGradle/app/src/main/java/jnw/android/trygradle/HelloWorldActivity.java
+++ TryGradle/app/src/main/java/jnw/android/trygradle/HelloWorldActivity.java
@@ -1,6 +1,9 @@
 package jnw.android.trygradle;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +11,7 @@ import android.widget.EditText;
 
 public class HelloWorldActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "jnw.android.trygradle.MESSAGE";
+    public final static int PICK_CONTACT_REQUEST = 17;
 
 
     @Override
@@ -25,5 +29,29 @@ public class HelloWorldActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
 
+    }
+
+    public void pickContact(View view) {
+        // Create an intent to "pick" a contact, as defined by the content provider URI
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // If the request went well (OK) and the request was PICK_CONTACT_REQUEST
+        if (resultCode == Activity.RESULT_OK && requestCode == PICK_CONTACT_REQUEST) {
+            // Perform a query to the contact's content provider for the contact's name
+            Cursor cursor = getContentResolver().query(data.getData(),
+                    new String[] {ContactsContract.Contacts.DISPLAY_NAME}, null, null, null);
+            if (cursor.moveToFirst()) { // True if the cursor is not empty
+                int columnIndex = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+                String name = cursor.getString(columnIndex);
+                // Do something with the selected contact's name...
+
+                EditText editText = (EditText) findViewById(R.id.edit_message);
+                editText.append(name);
+            }
+        }
     }
 }
