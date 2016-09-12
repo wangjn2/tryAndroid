0b5f3fabab5aa959e1a355f60337aafd18c89611
JNW <wangjn2@163.com>
2016-09-12 21:59:37 +0800

    add menu

--- TryGradle/app/src/main/java/jnw/android/trygradle/DisplayMessageActivity.java
+++ TryGradle/app/src/main/java/jnw/android/trygradle/DisplayMessageActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
@@ -30,5 +31,20 @@ public class DisplayMessageActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_activity_actions, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                // openSearch();
+                return true;
+            case R.id.action_settings:
+                // openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
